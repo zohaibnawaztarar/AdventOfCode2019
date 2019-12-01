9ad252c11289254ee52bd100a069e73b8401f4ab
Zohaib Nawaz Tarar <zohaib_nawaz41@yahoo.com>
2019-12-01 18:03:30 +0000

    The Tyranny of the Rocket Equation Completed.

---
+++ day1.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const input = fs.readFileSync(__dirname + '/input/day1', 'utf-8').trim().split("\n");
+const start = new Date().getTime();
+
+let part1 = 0, part2 = 0;
+
+input.forEach((item) => {
+    let value = ~~(item / 3) - 2;
+part1 += value;
+
+while (value > 0) {
+    part2 += value;
+    value = ~~(value / 3) - 2;
+}
+});
+
+console.log('Part 1: ' + part1);
+console.log('Part 2: ' + part2);
+
+console.log('Finished in: ' + (new Date().getTime() - start) + 'ms');
