7995acff3b9cc9f215ce4f006ee673f6fc6b6df2
Zohaib Nawaz Tarar <zohaib_nawaz41@yahoo.com>
2019-12-04 21:13:38 +0000

    Completed "Secure Container" - Day 4 - Advent of Code 2019

---
+++ day4.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const input = fs
+    .readFileSync(__dirname + '/input/day4', 'utf-8')
+    .trim()
+    .split('-');
+const start = new Date().getTime();
+
+let part1 = 0, part2 = 0;
+
+for (let num = +input[0], max = +input[1]; num <= max; num++) {
+    let sNum = num.toString();
+
+    if (sNum !== sNum.split('').sort().join('')) {
+        continue;
+    }
+
+    if (sNum.match(/(?:(?:(\d?)(?!\1))|^)(\d)\2(?!\2)/)) {
+        part1++;
+        part2++;
+    } else if (sNum.match(/(\d)\1/)) {
+        part1++;
+    }
+}
+
+console.log('Part 1: ' + part1);
+console.log('Part 2: ' + part2);
+
+console.log('Finished in: ' + (new Date().getTime() - start) + 'ms');
