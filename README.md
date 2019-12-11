595a128bd36f3db6531556d29403b658885b4637
Zohaib Nawaz Tarar <zohaib_nawaz41@yahoo.com>
2019-12-11 19:29:57 +0000

    Completed "Space Police" - Day 11 - Advent of Code 2019

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # AdventOfCode2019
 Solutions for [Advent of Code 2019](https://adventofcode.com/2019/) challenges
-### Completed 10/25
+### Completed 11/25
 
 - Day 1 - The Tyranny of the Rocket Equation
 - Day 2 - 1202 Program Alarm
-- Day 3 - Crossed Wires"
+- Day 3 - Crossed Wires
 - Day 4 - Secure Container
 - Day 5 - Sunny with a Chance of Asteroids
 - Day 6 - Universal Orbit Map
@@ -12,3 +12,4 @@ Solutions for [Advent of Code 2019](https://adventofcode.com/2019/) challenges
 - Day 8 - Space Image Format
 - Day 9 - Sensor Boost
 - Day 10 - Monitoring Station
+- Day 11 - Space Police
---
+++ day11.php
@@ -0,0 +1,45 @@
+<?php
+require_once __DIR__ . '/utils.php';
+$input = trim(file_get_contents(__DIR__ . '/input/day11'));
+$input = explode(',', $input);
+$start = microtime(true);
+$parseBlack = robot($input, 0);
+$part1 = count($parseBlack, COUNT_RECURSIVE) - count($parseBlack);
+$grid = robot($input, 1);
+$allX = $allY = [];
+foreach ($grid as $x => $yLine) {
+    $allX[] = $x;
+    foreach ($yLine as $y => $dummy) {
+        $allY[] = $y;
+    }
+}
+echo 'Part 1: ' . $part1 . PHP_EOL;
+echo 'Part 2: ' . PHP_EOL;
+for ($y = max($allY), $minY = min($allY); $y >= $minY; $y--) {
+    for ($x = min($allX), $maxX = max($allX); $x <= $maxX; $x++) {
+        echo $grid[$x][$y] ?? 0 == 1 ? '#' : ' ';
+    }
+    echo PHP_EOL;
+}
+echo 'Finished in ' . (microtime(true) - $start) . PHP_EOL;
+function robot($intCode, $startColor)
+{
+    $program = new IntCodeParser($intCode);
+    $dirMap = ['^' => [0 => '<', 1 => '>'], '>' => [0 => '^', 1 => 'v'], '<' => [0 => 'v', 1 => '^'], 'v' => [0 => '>', 1 => '<']];
+    $deltaX = ['^' => 0, '>' => 1, 'v' => 0, '<' => -1];
+    $deltaY = ['^' => 1, '>' => 0, 'v' => -1, '<' => 0];
+    $grid = [0 => [0 => $startColor]];
+    $x = $y = 0;
+    $dir = '^';
+    while (true) {
+        $program->input($grid[$x][$y] ?? 0);
+        $paint = $program->output();
+        if ($paint === null) {
+            return $grid;
+        }
+        $grid[$x][$y] = $paint;
+        $dir          = $dirMap[$dir][$program->output()];
+        $x            += $deltaX[$dir];
+        $y            += $deltaY[$dir];
+    }
+}
